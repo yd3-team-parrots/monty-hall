b2292cfb43cae6dc560d412839e0a72f86e61355
김준현 <joonhyeonkim1015@gmail.com>
2023-05-02 17:36:33 +0900

    Delete monty-hall.py

--- monty-hall.py
+++
@@ -1,9 +0,0 @@
-'''
-1. set the door that has a prize with random number generator
-2. ask user to choose the door for prize 
-3. computer to pick the door that does not have a prize
-4. ask user to choose whether one wants to change the choice
-'''
-
-import random
-
